d03d84b8f0b55f49ca7a3123512efb68d09dcbdf
Xiaoyi Liu <lxy878@gmail.com>
2020-03-09 20:50:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,8 +22,8 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    self.create_by_name(name)
-    self.find_by_name(name)
+    # self.create_by_name(name)
+    # self.find_by_name(name)
   end
   def self.all
     @@all
