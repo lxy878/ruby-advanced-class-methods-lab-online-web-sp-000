fbdbb54a7c97aed044fc791777bc5701d6eb8e1c
Xiaoyi Liu <lxy878@gmail.com>
2020-03-09 21:31:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,13 +48,13 @@ class Song
 
   def self.create_from_filename(filename)
     song = self.new
-    # fixed_filename = filename.gsub(/.mp3/)
-    # binding.pry
-    # parts = fixed_filename.split(" - ")
-    # song.name = parts[1]
-    # song.artist_name = parts[0]
-    # song.save
-    # song
+    fixed_filename = filename.gsub(/.mp3/)
+    binding.pry
+    parts = fixed_filename.split(" - ")
+    song.name = parts[1]
+    song.artist_name = parts[0]
+    song.save
+    song
   end
 
   def self.destroy_all
