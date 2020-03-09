2791a3aa18a8904db254243f22807b7eb0802864
Xiaoyi Liu <lxy878@gmail.com>
2020-03-09 21:40:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
     parts = filename.split(" - ")
     song = self.new
     song.name = parts[1]
-    binding.pry
+    # binding.pry
     song.artist_name = parts[0]
     song.save
     song
