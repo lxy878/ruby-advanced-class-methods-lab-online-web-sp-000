024a109296588a0181fe6c61bf1e058eae0032da
Xiaoyi Liu <lxy878@gmail.com>
2020-03-09 20:48:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,6 +20,11 @@ class Song
   def self.find_by_name(name)
     self.all.find {|song| song.name == name}
   end
+
+  def self.find_or_create_by_name(name)
+    self.create_by_name(name)
+    self.find_by_name(name)
+  end
   def self.all
     @@all
   end
